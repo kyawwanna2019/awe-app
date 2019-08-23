1902d13a707f8b35cd9abe0fa6573467bf4c37dd
kyawwanna2019 <kyawwanna2019@hotmail.com>
2019-08-23 12:09:35 +0700

    Second Commit with user-listing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListingComponent } from './user-listing/user-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-listing/user-listing.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-listing',
+  templateUrl: './user-listing.component.html',
+  styleUrls: ['./user-listing.component.scss']
+})
+export class UserListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
